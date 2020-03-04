2fdd58b75fbe50ab153158cf8154144072b3b53c
kanikas01 <larvakurt@yahoo.com>
2020-03-03 16:31:13 -0800

    Format duration to hh:mm:ss

--- src/components/SongPlayer.js
+++ src/components/SongPlayer.js
@@ -14,6 +14,7 @@ import { SongContext } from "../App";
 import { useQuery } from "@apollo/react-hooks";
 import { GET_QUEUED_SONGS } from "../graphql/queries";
 import ReactPlayer from "react-player";
+import { subscribe } from "graphql";
 
 const useStyles = makeStyles(theme => ({
   container: {
@@ -48,6 +49,7 @@ function SongPlayer() {
   const reactPlayerRef = React.useRef();
   const { state, dispatch } = React.useContext(SongContext);
   const [played, setPlayed] = React.useState(0);
+  const [playedSeconds, setPlayedSeconds] = React.useState(0);
   const [seeking, setSeeking] = React.useState(false);
   const classes = useStyles();
 
@@ -66,6 +68,10 @@ function SongPlayer() {
   function handleSeekMouseUp() {
     setSeeking(true);
     reactPlayerRef.current.seekTo(played);
+  }
+
+  function formatDuration(seconds) {
+    return new Date(seconds * 1000).toISOString().substr(11, 8);
   }
 
   return (
@@ -95,7 +101,7 @@ function SongPlayer() {
               <SkipNext />
             </IconButton>
             <Typography variant="subtitle1" component="p" color="textSecondary">
-              00:01:30
+              {formatDuration(playedSeconds)}
             </Typography>
           </div>
           <Slider
@@ -114,6 +120,7 @@ function SongPlayer() {
           onProgress={({ played, playedSeconds }) => {
             if (!seeking) {
               setPlayed(played);
+              setPlayedSeconds(playedSeconds);
             }
           }}
           url={state.song.url}
