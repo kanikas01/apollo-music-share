bc7ba43d6d0fa912ecfd1365f145b3c4fe05bf10
kanikas01 <larvakurt@yahoo.com>
2020-03-06 21:38:37 -0800

    Change secondary color, remove unused imports

--- src/theme.js
+++ src/theme.js
@@ -1,12 +1,13 @@
 import { createMuiTheme } from "@material-ui/core/styles";
-import { teal, purple } from "@material-ui/core/colors";
-import { dark } from "@material-ui/core/styles/createPalette";
+import { teal } from "@material-ui/core/colors";
 
 const theme = createMuiTheme({
   palette: {
     type: "dark",
     primary: teal,
-    secondary: purple
+    secondary: {
+      main: '#fff59d'
+    }
   }
 });
 
